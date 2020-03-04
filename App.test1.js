1560bfb2e30087b8bf255b198eb645c2029790b0
VladyslavZarubinWsiz <w48835@student.wsiz.rzeszow.pl>
2020-03-04 17:01:34 +0100

    closes #10440

--- App.test1.js
+++ App.test1.js
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App status />);
+  const { getByText } = render(<App status='solved' />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
